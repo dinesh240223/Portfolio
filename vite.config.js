6fafe09f387569ba8c962253bcee7b5947184735
DINESH S <s90960665@gmail.com>
2026-09-19 10:34:42 +0530

    Add multiple entry points to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        fullStack: resolve(__dirname, 'full-stack.html'),
+        cyber: resolve(__dirname, 'cyber.html'),
+        hardware: resolve(__dirname, 'hardware.html'),
+      },
+    },
   },
 })
